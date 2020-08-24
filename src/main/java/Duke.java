ae150dedbc656209994b69dad75961e1d244ae6e
luziyi9898 <luziyi9898@gmail.com>
2020-08-25 00:17:02 +0800

    added echo function. tweaked personality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+//import java.util.Arrays;
+import java.util.Scanner;
+
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        String inputStatement;
+        int endEchoFlag = 0;
+        String separatingLine = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+
+        //greeting lines start:
+        System.out.println(separatingLine);
+        System.out.println("Greetings, care for a cup of coffee?");
+        System.out.println(separatingLine);
+        //greeting lines end:
+
+        //echo loop begins:
+        while (endEchoFlag == 0) {
+            Scanner in = new Scanner(System.in);
+            inputStatement = in.nextLine();
+            if (!inputStatement.equalsIgnoreCase("bye")) {
+                System.out.println(separatingLine);
+                System.out.println("Did you say: " + inputStatement);
+                System.out.println(separatingLine);
+            }
+            else{
+                endEchoFlag = 1;
+            }
+        }
+        //echo loop ends
+        System.out.println(separatingLine);
+        System.out.println("We shall meet again...");
+        System.out.println(separatingLine);
+
+
+
+
+
+
+
     }
+
+
 }
